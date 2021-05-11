b654514c13bde2d6b1020584ae06c78ca06399a4
ujjwol75 <ujjwolneupane75@gmail.com>
2021-05-11 14:33:55 +0545

    initial commit

---
+++ app/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from app import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.auth import authenticate, login, views as auth_views
+from .forms import LoginForm 
+
+urlpatterns = [
+    path('', views.ProductView.as_view(), name='home'),
+    path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('buy/', views.buy_now, name='buy-now'),
+    path('profile/', views.profile, name='profile'),
+    path('address/', views.address, name='address'),
+    path('orders/', views.orders, name='orders'),
+    path('changepassword/', views.change_password, name='changepassword'),
+    path('mobile/', views.mobile, name='mobile'),
+    # path('login/', views.login, name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    # path('registration/', views.customerregistration, name='customerregistration'),
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+    path('checkout/', views.checkout, name='checkout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
